d6b4f1dc0f3c3847317391d691a7cc024b00b225
AshutoshDM1 <downlodemaster1@gmail.com>
2026-02-26 23:16:31 +0530

    feat: Initialize Bun tRPC server with a basic file router and project configuration.

---
+++ src/routers/file.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+import { router, publicProcedure } from "../trpc";
+
+export const fileRouter = router({
+  hello: publicProcedure
+    .input(z.object({ name: z.string() }))
+    .query(({ input }) => {
+      return {
+        message: `Hello ${input.name}`,
+      };
+    }),
+});
