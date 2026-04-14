ed022fb2546cc85c2f884e4a10a698fcdf887da9
AshutoshDM1 <downlodemaster1@gmail.com>
2026-04-14 21:59:46 +0530

    feat: pnpm workspace configuration

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,26 @@
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import express from "express";
+import cors from "cors";
+import { appRouter } from "@/appRouter";
+import { createContext } from "@/context";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+app.use("/trpc", createExpressMiddleware({ router: appRouter, createContext }));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
